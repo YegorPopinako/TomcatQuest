cf179894d75610ec5d009a181fc69d59b796718b
Yegor Popinako <yegor5x@gmail.com>
2024-06-22 15:55:32 +0300

    added retry logic

---
+++ src/main/java/ua/quest/servlet/RestartServlet.java
@@ -0,0 +1,18 @@
+package ua.quest.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "RestartServlet", value = "/restart")
+public class RestartServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.sendRedirect("/quest");
+    }
+}
---
+++ src/main/webapp/failure.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Failure</title>
+    <link rel="stylesheet" type="text/css" href="static/style.css">
+</head>
+<body class="failure">
+<div class="container">
+    <header class="header">
+        <h1>Failure</h1>
+    </header>
+    <main class="main-content">
+        <p>You lost. Try again</p>
+        <button onclick="window.location.href='/restart';">Retry</button>
+    </main>
+</div>
+
+</body>
+</html>
