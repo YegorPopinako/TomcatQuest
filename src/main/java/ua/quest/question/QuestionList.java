e2952e4dd63c3dd91ea4bf80cfbc320a65ac807b
Yegor Popinako <yegor5x@gmail.com>
2024-07-04 19:27:54 +0300

    fixed Question List

--- src/main/java/ua/quest/question/QuestionList.java
+++ src/main/java/ua/quest/question/QuestionList.java
@@ -2,24 +2,39 @@ package ua.quest.question;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class QuestionList {
 
-    private static List<Question> questions = new ArrayList();
+    private static List<Question> questions = new ArrayList<>();
 
-    static{
-        try(BufferedReader reader = Files.newBufferedReader(Path.of("C:\\Java\\Projects\\QuestTomcat\\src\\main\\resources\\questions"))){
+    private static final int QUESTION_INDEX = 0;
+    private static final int OPTION1_INDEX = 1;
+    private static final int OPTION2_INDEX = 2;
+    private static final int ANSWER_INDEX = 3;
+    private static final int WRONG_OPTION_RESULT_INDEX = 4;
+
+    public static final String DELIMITER = "\\|";
+
+    static {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(QuestionList.class.getClassLoader().getResourceAsStream("questions"))))) {
             String line;
-            while((line = reader.readLine()) != null){
-                String[] parts = line.split("\\|");
-                questions.add(new Question(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]), parts[4]));
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(DELIMITER);
+                questions.add(new Question(
+                        parts[QUESTION_INDEX],
+                        parts[OPTION1_INDEX],
+                        parts[OPTION2_INDEX],
+                        Integer.parseInt(parts[ANSWER_INDEX]),
+                        parts[WRONG_OPTION_RESULT_INDEX]
+                ));
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.getCause();
         }
     }
 
